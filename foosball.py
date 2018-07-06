440b2359c7132f831bd4d95d75251abe238ea8d7
Chris Rudolph <christopher.rudolph@gmail.com>
2018-07-05 19:56:13 -0600

    Solved foosball

---
+++ foosball.py
@@ -0,0 +1,66 @@
+# https://open.kattis.com/problems/foosball
+
+import sys
+
+DEBUG = False
+
+
+def dbg(str):
+    if DEBUG:
+        print(str, file=sys.stderr)
+
+
+player_count = int(input())
+players = input().split()
+result_str = input()
+
+dbg(f"PlayerCount:{player_count}")
+dbg(f"Players:{players}")
+dbg(f"ResultStr:{result_str}")
+
+sides = [
+    [players[0], players[2]],
+    [players[1], players[3]]
+]
+line = players[4:]
+
+dbg(f"Side[0]:{sides[0]}")
+dbg(f"Side[1]:{sides[1]}")
+dbg(f"Line:{line}")
+
+win_tracking = [
+    [" ".join(sides[0][:]), 0],
+    [" ".join(sides[1][:]), 0]
+]
+
+best = 0
+dynasties = []
+
+for c in result_str:
+    winner_idx = 0 if c == "W" else 1
+    loser_idx = 1 if winner_idx == 0 else 0
+
+    winner = sides[winner_idx]
+    winner_rec = win_tracking[winner_idx]
+    loser = sides[loser_idx]
+    loser_rec = win_tracking[loser_idx]
+
+    winner[0], winner[1] = winner[1], winner[0]
+    winner_rec[1] += 1
+    dbg(f"Win -- Team:{winner_rec[0]}; Wins:{winner_rec[1]}")
+
+    if winner_rec[1] > best:
+        best = winner_rec[1]
+        dynasties = [winner_rec[0]]
+        dbg(f"New Best -- Team:{winner_rec[0]}, Wins:{winner_rec[1]}")
+    elif winner_rec[1] == best:
+        dynasties.append(winner_rec[0])
+        dbg(f"New Tie -- Team:{winner_rec[0]}, Wins:{winner_rec[1]}")
+
+    line.append(loser[1])
+    loser[1] = loser[0]
+    loser[0] = line.pop(0)
+    win_tracking[loser_idx] = [loser[1] + " " + loser[0], 0]
+    dbg(f"New Losing Team: {win_tracking[loser_idx][0]}")
+
+print("\n".join(dynasties))
