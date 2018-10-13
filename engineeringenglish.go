d69223719ff425635a0a095075f50f8a3552c6c8
Chris Rudolph <christopher.rudolph@gmail.com>
2018-10-12 21:12:24 -0600

    Solved engineeringenglish (go)

---
+++ engineeringenglish.go
@@ -0,0 +1,32 @@
+// https://open.kattis.com/problems/engineeringenglish
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	m := make(map[string]bool)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		s := scanner.Text()
+		words := strings.Fields(s)
+		for i, f := range words {
+			// Use a consistent case since the problem is case insensitive
+			cased := strings.ToUpper(f)
+			_, ok := m[cased]
+			if ok {
+				// We've seen this word before. Replace it
+				words[i] = "."
+			}
+			m[cased] = true
+		}
+
+		fmt.Println(strings.Join(words, " "))
+	}
+}
