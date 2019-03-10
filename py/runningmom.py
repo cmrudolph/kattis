efc6fdb61bbf887f9f790a58d55a3b592911f1e9
Chris Rudolph <christopher.rudolph@gmail.com>
2019-03-09 20:06:44 -0700

    Recursive (wrong answer)

--- py/runningmom.py
+++ py/runningmom.py
@@ -3,9 +3,6 @@
 # =============================================================================
 # Weighted graph problem where DFS can be used to identify cycles and the
 # cycles serve as the basis for identifying "safe" cities in the analysis.
-# Recursion doesn't fly here because of stack size limits, so an iterative
-# approach is used. History tracking is required since we need to be able to
-# trace the path we have taken when analyzing cycles.
 # =============================================================================
 
 import sys
@@ -16,37 +13,33 @@ def dbg(str):
     print(str, file=sys.stderr)
 
 
-def dfs(graph, all_visited, safe, start):
-    # Iterative DFS to avoid recursion limits
-    stack = [(start, [start])]
-    hist_set = {start}
-    while len(stack) > 0:
-        top = stack[-1]
-        top_vertex = top[0]
-        top_history = top[1]
-        if top_vertex in all_visited:
-            # Avoid redundant work.
-            stack.pop()
-        else:
-            all_visited.add(top_vertex)
-            for neighbor in graph.get(top_vertex, set()):
-                if neighbor in hist_set:
-                    cycle_detected = False
-                    for hist in top_history:
-                        if hist == neighbor:
-                            # Found a cycle. All cities from the first sighting to
-                            # the second sighting are considered safe.
-                            cycle_detected = True
-                        if cycle_detected:
-                            if hist not in safe:
-                                safe.add(hist)
-                if neighbor not in all_visited:
-                    # Unvisited neighbor - push it to visit later.
-                    stack.append((neighbor, top_history + [neighbor]))
-                    hist_set.add(neighbor)
+def recurse(graph, all_visited, curr_visited, safe, curr_key):
+    if curr_key in all_visited:
+        return
+
+    if curr_key in curr_visited:
+        # Cycle detected. All cities participating in this cycle are safe.
+        # This set consists of all cities between the first and second
+        # sighting of the city we just noticed for a second time.
+        seen_curr = False
+        for s in curr_visited:
+            if s == curr_key:
+                seen_curr = True
+            if seen_curr and s not in safe:
+                safe.add(s)
+        return
+
+    # Recursively visit the connected nodes in the graph
+    curr_visited.append(curr_key)
+    if curr_key in graph:
+        for dest in graph[curr_key]:
+            recurse(graph, all_visited, curr_visited, safe, dest)
+    curr_visited.pop()
+    all_visited.add(curr_key)
 
 
 def main():
+    sys.setrecursionlimit(10000)
     next_idx = 0
     n = int(input())
     city_to_idx = dict()
@@ -55,10 +48,10 @@ def main():
         o, d = input().strip().split()
 
         # Map names to integers to make keyed lookups faster
-        if o not in city_to_idx:
+        if not o in city_to_idx:
             city_to_idx[o] = next_idx
             next_idx += 1
-        if d not in city_to_idx:
+        if not d in city_to_idx:
             city_to_idx[d] = next_idx
             next_idx += 1
 
@@ -70,8 +63,8 @@ def main():
     curr_visited = []
     safe = set()
 
-    for start in graph:
-        dfs(graph, all_visited, safe, start)
+    for key in graph:
+        recurse(graph, all_visited, curr_visited, safe, key)
 
     for line in sys.stdin:
         line = line.strip()
