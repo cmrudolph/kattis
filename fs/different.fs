02d73c78e5434e58527690ca0af7e574a814805b
Chris Rudolph <christopher.rudolph@gmail.com>
2020-10-04 19:44:22 -0600

    F# sample problem to test plumbing

---
+++ fs/different.fs
@@ -0,0 +1,22 @@
+// NAME : A Different Problem
+// URL  : https://open.kattis.com/problems/different
+// =============================================================================
+// Test problem to get F# environment working
+// =============================================================================
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    (fun _ -> Console.ReadLine()) |>
+    Seq.initInfinite |>
+    Seq.takeWhile ((<>) null) |>
+    Seq.iter
+       (fun (s : string) ->
+            let arr = s.Split([|' '|])
+            let a = int64 arr.[0]
+            let b = int64 arr.[1]
+            /// solve test case and output answer
+            printfn "%d" (abs (a - b))
+       )
+    0
